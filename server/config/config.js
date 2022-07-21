69fdede7cb2919671d6abfa1034608e6a57ce8c7
Andile-coder <maselaandile@gmail.com>
2022-07-21 16:39:21 +0200

    ---

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "clouduser",
   host: "localhost",
-  database: "mw_prox",
+  database: "mw_prox_api",
   password: "clouduser",
   port: 5432,
 });
